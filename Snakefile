52249278718802fe949ada003ae14615db73c36f
Roy <roy@cronus010>
2021-10-27 14:52:08 +0200

    Addition of probe set permutations

--- Snakefile
+++ Snakefile
@@ -22,37 +22,6 @@ rule extract_target_sequences:
 		"bedtools getfasta -fi {input.genome} -bed {input.bed}  -fo {output}"
 
 #---------------------------------------------------------------------------------------------------
-#STEP 2: FIND CpG AND SNP IN TARGET SEQUENCES
-
-rule convert_bed_file: #Convert the chromosome names from chr1 to NC_000001.10 format to be compatible with the ftp assembly in the find_overlapping_VCF rule
-#https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.25_GRCh37.p13/GCF_000001405.25_GRCh37.p13_assembly_report.txt
-        input:
-                config['output_directory']+"/target_list_range.bed"
-        output:
-                config['output_directory']+"/target_list_range_conv.bed"
-        shell:
-                "sed -e 's/chr1\t/NC_000001.10\t/' {input} | sed -e 's/chr2\t/NC_000002.11\t/' | "
-                "sed -e 's/chr3\t/NC_000003.11\t/' |sed -e 's/chr4\t/NC_000004.11\t/' | "
-                "sed -e 's/chr5\t/NC_000005.9\t/' |sed -e 's/chr6\t/NC_000006.11\t/' | "
-                "sed -e 's/chr7\t/NC_000007.13\t/' |sed -e 's/chr8\t/NC_000008.10\t/' | "
-                "sed -e 's/chr9\t/NC_000009.11\t/' |sed -e 's/chr10\t/NC_000010.10\t/' | "
-                "sed -e 's/chr11\t/NC_000011.9\t/' |sed -e 's/chr12\t/NC_000012.11\t/' | "
-                "sed -e 's/chr13\t/NC_000013.10\t/' |sed -e 's/chr14\t/NC_000014.8\t/' | "
-                "sed -e 's/chr15\t/NC_000015.9\t/' |sed -e 's/chr16\t/NC_000016.9\t/' | "
-                "sed -e 's/chr17\t/NC_000017.10\t/' |sed -e 's/chr18\t/NC_000018.9\t/' | "
-                "sed -e 's/chr19\t/NC_000019.9\t/' |sed -e 's/chr20\t/NC_000020.10\t/' | "
-                "sed -e 's/chr21\t/NC_000021.8\t/' |sed -e 's/chr22\t/NC_000022.10\t/' | "
-                "sed -e 's/chrX\t/NC_000023.10\t/' |sed -e 's/chrY\t/NC_000024.9\t/' "
-                "> {output}"
-
-rule find_overlapping_VCF:#Obtain all known SNPs 
-	input:
-		config['output_directory']+"/target_list_range_conv.bed"
-	output:
-		config['output_directory']+"/targets.vcf"	
-	shell:
-		"tabix ftp://ftp.ncbi.nih.gov/snp/latest_release/VCF/GCF_000001405.25.gz -R {input} > {output}"
-
 rule keep_high_freq_SNP: #Keep the SNPs that have a frequency that is equal or higher than the threshold in the config file:SNP_frequency_threshold.
 	input:
 		config['output_directory']+"/targets.vcf"
@@ -60,38 +29,6 @@ rule keep_high_freq_SNP: #Keep the SNPs that have a frequency that is equal or h
 		config['output_directory']+"/high_targets.vcf"
 	shell:
 		"python scripts/extract_high_frequent_SNPs.py -i {input} -o {output} -f {config[SNP_frequency_threshold]}"
-
-rule find_CpGs_in_targets:
-	input:
-		config['output_directory']+"/target_list_range.bed"
-	output:
-		config['output_directory']+"/CpG_in_targets.bed"
-	shell:
-		"intersectBed -a {input} -b data/CpG_bed_nomenclature/CpG_hg19_chr* -wa -wb> {output}"
-
-rule convert_vcf_file:
-#https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.25_GRCh37.p13/GCF_000001405.25_GRCh37.p13_assembly_report.txt
-        input:
-                config['output_directory']+"/high_targets.vcf"
-        output:
-                tmp_snp=config['output_directory']+"/SNP_in_targets.vcf",
-		snp=config['output_directory']+"/SNP_in_targets.bed"
-        shell:
-                "sed -e 's/NC_000001.10\t/chr1\t/' {input} | sed -e 's/NC_000002.11\t/chr2\t/' | "
-                "sed -e 's/NC_000003.11\t/chr3\t/' |sed -e 's/NC_000004.11\t/chr4\t/' | "
-                "sed -e 's/NC_000005.9\t/chr5\t/' |sed -e 's/NC_000006.11\t/chr6\t/' | "
-                "sed -e 's/NC_000007.13\t/chr7\t/' |sed -e 's/NC_000008.10\t/chr8\t/' | "
-                "sed -e 's/NC_000009.11\t/chr9\t/' |sed -e 's/NC_000010.10\t/chr10\t/' | "
-                "sed -e 's/NC_000011.9\t/chr11\t/' |sed -e 's/NC_000012.11\t/chr12\t/' | "
-                "sed -e 's/NC_000013.10\t/chr13\t/' |sed -e 's/NC_000014.8\t/chr14\t/' | "
-                "sed -e 's/NC_000015.9\t/chr15\t/' |sed -e 's/NC_000016.9\t/chr16\t/' | "
-                "sed -e 's/NC_000017.10\t/chr17\t/' |sed -e 's/NC_000018.9\t/chr18\t/' | "
-                "sed -e 's/NC_000019.9\t/chr19\t/' |sed -e 's/NC_000020.10\t/chr20\t/' | "
-                "sed -e 's/NC_000021.8\t/chr21\t/' |sed -e 's/NC_000022.10\t/chr22\t/' | "
-                "sed -e 's/NC_000023.10\t/chrX\t/' |sed -e 's/NC_000024.9\t/chrY\t/' "
-                "> {output.tmp_snp} && "
-		"""cat {output.tmp_snp} | awk -F"\\t" "BEGIN {{OFS = FS}}{{print \$1,\$2-1,\$2,\$0}}" | cut -f -3,6- > {output.snp}"""
-
 #---------------------------------------------------------------------------------------------------
 #STEP 3
 rule create_all_arm_combinations:
--- scripts/create_all_arm_combinations.py
+++ scripts/create_all_arm_combinations.py
@@ -7,6 +7,7 @@ from Bio import SeqIO, Seq, SeqRecord
 from Bio.SeqUtils import GC
 import subprocess
 import multiprocessing as mp
+from numpy.random import choice
 
 parser = ArgumentParser()
 parser.add_argument("-i", "--input", dest="filename",
@@ -43,7 +44,8 @@ max_CG_percentage=float(probe_specifics['max_CG_percentage'])
 min_CG_percentage=float(probe_specifics['min_CG_percentage'])
 backbone_sequence=configObject["Backbone_sequence"]
 mid_loc=int(target_range)+1 #The middle nucleotide is the target range +1 because the total target is created by adding the target range on both sides.
-
+freq_threshold=float(configObject['SNP_frequency_threshold'])
+permutations=int(configObject['Permutations'])
 def create_all_possible_arms(record,min_target_length,max_target_length,min_arm_length,max_arm_length,min_CG_percentage,max_CG_percentage,cpg_flanks,mid_loc):
     probe_list=[]
     i=0
@@ -141,8 +143,8 @@ def get_delta_Tm(probe_arms): #Wallace rule from (Abdul-Latiff et al., 2017)
     G=downstream_arm.count('G')
     C=downstream_arm.count('C')
     Tm_down=64.9+41*(G+C-16.4)/(A+T+C+G)
-    delta_Tm=abs(Tm_up-Tm_down)
-    return delta_Tm
+    delta_Tm=round(abs(Tm_up-Tm_down),1)
+    return [Tm_up,Tm_down,delta_Tm]
 
 def report_CpGs_in_arms(probe_arms):
     upstream_arm=probe_arms[0]
@@ -182,35 +184,172 @@ def check_probe_for_hairpin_score(probe_list,fasta_name,outputname_json):
             hairpin_scores[i][j]=score
     return hairpin_scores
 
+def loci_to_bed_37(locus):#https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.25_GRCh37.p13/GCF_000001405.25_GRCh37.p13_assembly_report.txt
+    chrom=locus.split(':')[0][3:]
+    left_index=locus.split(':')[1].split('-')[0]
+    right_index=locus.split('-')[1]
+    chromnrlist=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','X','Y']
+    chromlist=['NC_000001.10','NC_000002.11','NC_000003.11','NC_000004.11','NC_000005.9','NC_000006.11','NC_000007.13','NC_000008.10','NC_000009.11','NC_000010.10','NC_000011.9','NC_000012.11','NC_000013.10','NC_000014.8','NC_000015.9','NC_000016.9','NC_000017.10','NC_000018.9','NC_000019.9','NC_000020.10','NC_000021.8','NC_000022.10','NC_000023.10','NC_000024.9']
+    bed_info=[chromlist[chromnrlist.index(chrom)],left_index,right_index]
+    return bed_info
+
+def target_bed_to_bed_37(bedpath):
+    chromnrlist=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','X','Y']
+    chromlist=['NC_000001.10','NC_000002.11','NC_000003.11','NC_000004.11','NC_000005.9','NC_000006.11','NC_000007.13','NC_000008.10','NC_000009.11','NC_000010.10','NC_000011.9','NC_000012.11','NC_000013.10','NC_000014.8','NC_000015.9','NC_000016.9','NC_000017.10','NC_000018.9','NC_000019.9','NC_000020.10','NC_000021.8','NC_000022.10','NC_000023.10','NC_000024.9']
+    bed_info_list=[]
+    with open(bedpath) as handle:
+        reader=csv.reader(handle,delimiter='\t')
+        for row in reader:
+            chrom=row[0][3:]
+            bed_info=[chromlist[chromnrlist.index(chrom)],row[1],row[2],row[3]]
+            bed_info_list.append(bed_info)
+    new_path=bedpath+'_37'
+    with open(new_path, 'w') as output_handle:
+        writer=csv.writer(output_handle,delimiter='\t')
+        writer.writerows(bed_info_list)
+    return new_path
+
+def obtain_SNPs(probe_list,bedpath,freq_threshold):
+    #first obtain frequent SNPs in targets
+    new_path=target_bed_to_bed_37(bedpath)
+    os.system('tabix ftp://ftp.ncbi.nih.gov/snp/latest_release/VCF/GCF_000001405.25.gz -R '+new_path+' > tmp_output_snp') #put this FTP URL in the config file?
+    chr_list=[]
+    loc_list=[]
+    freq_threshold=1.0-freq_threshold
+    with open('tmp_output_snp') as inputfile:
+        input_reader=csv.reader(inputfile)
+        for row in input_reader:
+            if "FREQ" in row[0]:
+                freq=float(row[0].split(";")[-1].split(":")[-1])
+                if freq < freq_threshold:
+                    chrom=row[0].split('\t')[0]
+                    locus=int(row[0].split('\t')[1])
+                    if chrom in chr_list:
+                        loc_list[chr_list.index(chrom)].append(locus)
+                    else:
+                        chr_list.append(chrom)
+                        loc_list.append([locus])
+    SNP_count=[[0 for probe in probe_arms] for probe_arms in probe_list]
+    #first create a bedfile containing each of the arms
+    arm1list=[]
+    arm2list=[]
+    for i,probe_arms in enumerate(probe_list):
+        for j,probe in enumerate(probe_arms):
+            arm1=loci_to_bed_37(probe[2])
+            arm1.append(str(i)+'-'+str(j))
+            arm2=loci_to_bed_37(probe[3])
+            arm2.append(str(i)+'-'+str(j))
+            if arm1[0] in chr_list:
+                index_to_check=chr_list.index(arm1[0])
+                for locus in loc_list[index_to_check]:
+                    if locus in range(int(arm1[1]),int(arm1[2])+1): #check whether the SNP is in the arm
+                        SNP_count[i][j]+=1
+                    if locus in range(int(arm2[1]),int(arm2[2])+1): #check whether the SNP is in the arm
+                        SNP_count[i][j]+=1
+    return SNP_count
+
+def get_probe_scores(probe_arms,tms,CpG_conflicts,SNP_conflicts,hairpin_score):
+    probe_score=hairpin_score*10+CpG_conflicts*10+SNP_conflicts*10+tms[2]
+    return probe_score
+
+def choose_probes_from_scores(probe_scores,possible_arm_combinations,n,counter):
+    if possible_arm_combinations==[]:
+        return
+    else:
+        sum_score=sum(probe_scores)
+        probability_distribution=[i/sum_score for i in probe_scores]
+        probe= choice(possible_arm_combinations,n,p=probability_distribution)
+    return probe[counter]
+
+def find_dimer_forming_probes(chosen_probes):
+    #Create a fasta file with the chosen probes
+    passed_list=[]
+    with open('tmp_fasta_chosen_probes.fasta','w') as handle:
+        for i,probe in enumerate(chosen_probes):
+            if probe ==None:
+                passed_list.append(i)
+            else:
+                handle.write('>'+str(i)+'\n')
+                handle.write(probe+'\n')
+    #test the chosen probes set for dimers
+    os.system('mfeprimer dimer -i tmp_fasta_chosen_probes.fasta -j -o tmp_dimers_chosen_probes')
+    os.system('rm tmp_fasta_chosen_probes.fasta tmp_dimers_chosen_probes')
+    dimers=[False for i in range(len(chosen_probes))]
+    with open('tmp_dimers_chosen_probes.json') as handle:
+        reader = json.load(handle)
+        jsonFile.close()
+        for row in reader:
+            index1=int(row['S1']['ID'])
+            dimers[index1]=True
+            index2=int(row['S2']['ID'])
+            dimers[index2]=True
+    #obtain which probes form dimers --> No dimer=False, dimer=True
+    return dimers
 #----------------------------------------------------------------------------------------------------------
 
 with open(filename) as handle:
     pool=mp.Pool(nr_of_cores)#mp.cpu_count())
     possible_arm_combinations_all_targets=[pool.apply(create_all_possible_arms_both_strands,args=(record,min_target_length,max_target_length,min_arm_length,max_arm_length,min_target_length,max_CG_percentage,cpg_flanks,mid_loc)) for record in SeqIO.parse(handle,"fasta")]
     pool.close()
-
+print('All possible arms obtained')
 pool=mp.Pool(nr_of_cores)
 tms=[[pool.apply(get_delta_Tm, args=([probe_arms])) for probe_arms in possible_arm_combinations] for possible_arm_combinations in possible_arm_combinations_all_targets]
 pool.close()
-
+print('\tTms obtained')
 pool=mp.Pool(nr_of_cores)
 CpG_conflicts=[[pool.apply(report_CpGs_in_arms, args=([probe_arms])) for probe_arms in possible_arm_combinations] for possible_arm_combinations in possible_arm_combinations_all_targets]
 pool.close()
-
+print('\tCpGs in arms counted')
 pool=mp.Pool(nr_of_cores)
 possible_probes_all_targets=[[pool.apply(add_backbone, args=(probe_arms,backbone_sequence)) for probe_arms in possible_arm_combinations] for possible_arm_combinations in possible_arm_combinations_all_targets]
 pool.close()
-
+print('\tBackbone added')
 fasta_name='temp_test_fasta.fa'
 outputname_json='temp_test_json'
 hairpin_scores=check_probe_for_hairpin_score(possible_probes_all_targets,fasta_name,outputname_json)
-#to implement
-    #SNPs in arms
-    #hairpin-score
+print('\tHairpins detected')
+
+SNP_conflicts=obtain_SNPs(possible_arm_combinations_all_targets,bedfile,freq_threshold)
+print('\tSNPs in arms counted')
+
+pool=mp.Pool(nr_of_cores)
+probe_scores=[[pool.apply(get_probe_scores,args=(probe_arms,tms[i][j],CpG_conflicts[i][j],SNP_conflicts[i][j],hairpin_scores[i][j])) for j,probe_arms in enumerate(possible_arm_combinations)] for i,possible_arm_combinations in enumerate(possible_probes_all_targets)] #function that scores each probe based on Tms, CpG/SNP conflicts and hairpin score
+pool.close()
+print('\tProbe scores computed')
+
+#print(probe_scores)
+
+counter=0
+chosen_probes_lists=[]
+probes_with_dimers_lists=[]
+while counter < permutations:
+    # put the below lines in a function that can be called ~100 times to be able to find the best subset of probes.
+    pool=mp.Pool(nr_of_cores)
+    chosen_probes=[]
+    chosen_probes=[pool.apply(choose_probes_from_scores,args=(probe_scores[i],possible_arm_combinations,permutations,counter)) for i,possible_arm_combinations in enumerate(possible_probes_all_targets)] #function that choses probes by the probability which is based on the score
+    pool.close()
+    print('\tProbes chosen')
+    chosen_probes_lists.append(chosen_probes)
+    probes_with_dimers=find_dimer_forming_probes(chosen_probes) #function that returns for each chosen probe whether if forms a dimer with one of the other chosen probes
+    probes_with_dimers_lists.append(probes_with_dimers)
+    nr_of_dimer_probes=sum(probes_with_dimers)
+    print('\t'+str(nr_of_dimer_probes)+' probes form a dimer')
+    # put the above lines in a function that can be called ~100 times to be able to find the best subset of probes.
+    counter+=1
+
+
+#print(probes_with_dimers_lists)
+#print(chosen_probes_lists)
+#to write
+#    get_probe_scores
+#    choose_probes_from_scores
+#    find_dimer_forming_probes
 
 #test print first probe. First arm combinations from first target site.
-print(tms[0][0])
-print(possible_arm_combinations_all_targets[0][0])
-print(CpG_conflicts[0][0])
-print(possible_probes_all_targets[0][0])
-print(hairpin_scores)
+#print(tms)
+#print(possible_arm_combinations_all_targets[0][0])
+#print(CpG_conflicts)
+#print(possible_probes_all_targets[0][0])
+#print(hairpin_scores)
+#print(SNP_conflicts)
+
